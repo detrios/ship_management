4a300f8037c315a57771d936cd8bc8050487d781
detrios <developpeurntic@gmail.com>
2014-02-13 13:00:39 +0100

    tt

--- js/main.js
+++ js/main.js
@@ -4,25 +4,39 @@ function onDeviceReady() {
 document.addEventListener("deviceready", onDeviceReady, false);
 
 $(document).ready(function (){
-
-$.ajax({
-   type: 'GET',
-    url: 'http://vps36292.ovh.net/mordu/API_2.7.php',
-    jsonpCallback: 'API_SC',
-    contentType: "application/json",
-    dataType: 'jsonp',
-    data:'action=org&team=306THSP&page=1',
-    async:true,
-    success: function(data) {
-       $('#member_guilde').html('');
-		for(var i =0; i< data.nb_membre; i++){
-			$('#member_guilde').append('<img src="http://robertsspaceindustries.com'+data[i].avatar+'" />'+data[i].pseudo+'<br style="clear:both" />');
+	
+	$('#search_orga').click(function(){
+		var team = '306THSP';
+		$('#member_guilde').html('<img src="css/themes/images/icons-png/clock-white.png" />');
+		if($('#orga_name').val()){
+			team = $('#orga_name').val();
 		}
-    },
-    error: function(e) {
-       console.log(e.message);
-    }
-});
+		$.ajax({
+			   type: 'GET',
+			    url: 'http://vps36292.ovh.net/mordu/API_2.7.php',
+			    jsonpCallback: 'API_SC',
+			    contentType: "application/json",
+			    dataType: 'jsonp',
+			    data:'action=org&team='+team+'&page=1',
+			    async:true,
+			    success: function(data) {
+			       $('#member_guilde').html('');
+			       if(data.nb_membre>0){
+					for(var i =0; i< data.nb_membre; i++){
+						$('#member_guilde').append('<img src="http://robertsspaceindustries.com'+data[i].avatar+'" />'+data[i].pseudo+'<br style="clear:both" />');
+					}
+			       }
+			       else {
+			    	   $('#member_guilde').append('cette organisation na aucun membre, etes vous sur de son tag?');
+			       }
+			    },
+			    error: function(e) {
+			       console.log(e.message);
+			    }
+			});
+	});
+
+
 
 });
 
